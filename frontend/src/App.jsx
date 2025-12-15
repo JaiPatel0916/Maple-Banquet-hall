bc2bf9f93850a4f44d01a4da18d42fd26b8ad054
jai099 <131276939+jai099@users.noreply.github.com>
2025-12-15 12:12:59 +0530

    tailwind working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
 
-        <h1 className="text-4xl font-bold text-blue-500">Maple Banquet Hall</h1>
+        <h1 className="text-4xl font-bold text-yellow-500">Maple Banquet Hall</h1>
 
         
       </div>
