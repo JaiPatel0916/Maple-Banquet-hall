593b6ffbb8d9a691b37f04043630a3d21bc4e820
Kartikmohod123 <kartikm.cse22@sbjit.edu.in>
2025-12-19 17:25:58 +0530

    hero section edit

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,12 +2,13 @@ import AboutStory from "../components/About/AboutStory";
 import AboutValues from "../components/About/AboutValues";
 import FounderPreview from "../components/About/FounderPreview";
 import VisionMission from "../components/About/VisionMission";
-
+import AboutHeroSection from "../components/About/AboutHeroSection"
 
 export default function About() {
     return (
         <>
             <div >
+                <AboutHeroSection/>
                 <AboutStory />
                 <AboutValues />
                 <FounderPreview />
