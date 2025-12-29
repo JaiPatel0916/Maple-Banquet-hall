63c14c264e46ff94a42e510965ff27aa5ea70d3d
jai099 <131276939+jai099@users.noreply.github.com>
2025-12-29 15:00:56 +0530

    axios.js changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-const isLocal =
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1";
-
 const api = axios.create({
-    baseURL: isLocal
-        ? "http://localhost:5001/api"
-        : "https://maple-banquet-hall.vercel.app/api",
+    baseURL: "https://maple-banquet-hall.vercel.app/api",
 });
 
 export default api;
